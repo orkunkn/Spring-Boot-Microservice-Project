0e6fb99097684d5c3869ace5dfff8d331b5623a4
orkunkn <orkunkaymann@gmail.com>
2023-08-09 17:10:00 +0300

    Added distributed tracing using Sleuth and Zipkin

--- microservices-parent/microservices/inventory-service/src/main/java/com/orkunkn/inventoryservice/service/InventoryService.java
+++ microservices-parent/microservices/inventory-service/src/main/java/com/orkunkn/inventoryservice/service/InventoryService.java
@@ -3,8 +3,6 @@ package com.orkunkn.inventoryservice.service;
 import com.orkunkn.inventoryservice.dto.InventoryResponse;
 import com.orkunkn.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,17 +10,12 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode) {
-        log.info("Wait Started");
-        Thread.sleep(10000);
-        log.info("Wait Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                     InventoryResponse.builder().skuCode(inventory.getSkuCode())
